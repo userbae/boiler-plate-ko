47c18b8257b90e7baac6f976b26fa8dcf1ab77dd
배찬혁 <new3930@gmail.com>
2023-03-17 17:28:54 +0900

    수정

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const { User } = require('./models/User');
+const bodyParser  = require('body-parser');
+const config = require('./config/key')
 
-const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://new:new960501@cluster0.b1x3oww.mongodb.net/?retryWrites=true&w=majority').then(() => console.log('MongoDB Connected...')).catch(err => console.log(err))
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.json());
+
+
+
+const mongoose = require('mongoose');
+mongoose.connect(config.mongoURI).then(() => console.log('MongoDB Connected...')).catch(err => console.log(err))
 
 
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/register', (req, res) => {
+  //회원 가입 할때 필요한 정보들을 client에서 가져오면
+  //그것들을 데이터 베이스에 넣어준다.
+  const user = new User(req.body);
+
+  user.save().then(()=>{
+    res.status(200).json({
+        success:true
+    })}).catch((err)=>{
+        return res.json({success:false,err})
+    });
 })
 
 app.listen(port, () => {
--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const userSchema = mongoose.Schem({
+const userSchema = mongoose.Schema({
   name: {
     type: String,
     maxlength: 50
